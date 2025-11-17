3281a27cbcb511883127e9c8a6afb43248438ec9
weng <weng0@hm.edu>
2025-11-18 00:07:43 +0100

    Add new function print_gameover() and if_gameover().

--- src/game_interface.py
+++ src/game_interface.py
@@ -5,13 +5,7 @@ class Game_Interface:
         self.punktzahl = 0
         self.spieler_name = ''
     
-    def setName(self): pass#, fn_stdscr pass
-        # wort = ''
-
-        # fn_stdscr.addstr(10, 85, 'Bitte geben Sie Ihre Spielername ein')
-        # while 
-        # buchstabe = input()
-        # return self.spieler_name
+    def setName(self, stdscr_fn, curses_fn): pass
 
 
     def update_Punktzahl(self, cluster_f : ClusterFest):
@@ -19,3 +13,25 @@ class Game_Interface:
 
     def print_Interface(self, stdscr_fn):
         stdscr_fn.addstr(10, 55, f"Punktzahl: {self.punktzahl}")
+
+    def if_gameover(self,  cluster_f : ClusterFest): # Für GAMEOVER
+        if cluster_f.pruef_ob_max_Hoehe():
+            return True
+        
+    def print_gameover(self, stdscr_fn,curses_fn): # Für GAMEOVER
+        stdscr_fn.addstr(10, 1, "/////     //  //    //  /////   //  /     / ///// ////")
+        stdscr_fn.addstr(11, 1, "//       / /  / /  / /  /___  /   /  /   /  /___  /  /")
+        stdscr_fn.addstr(12, 1, "/  ///  ///// /  //  /  /     /   /   / /   /     ////")
+        stdscr_fn.addstr(13, 1, "////   /   // // /   // /////  //      /    ///// /  // ")
+
+        stdscr_fn.addstr(17, 10, f"Punktzahl: {self.punktzahl}")
+
+
+
+
+'''
+/////     //  //    //  /////   //  /     / ///// ////
+//       / /  / /  / /  /___  /   /  /   /  /___  /  /
+/  ///  ///// /  //  /  /     /   /   / /   /     ////
+////   /   // // /   // /////  //      /    ///// /  // 
+'''
