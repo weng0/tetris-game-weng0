f399d4697c6f31cd4f6537d4485bc9e4e9e93344
weng0 <weng0@hm.edu>
2025-12-16 00:34:32 +0100

    Complete setName() function, add object of ClusterFest as second parameter to if_gameover(), add another parameter to print_gameover() and print name of the player when game over is printed.

--- src/game_interface.py
+++ src/game_interface.py
@@ -5,8 +5,12 @@ class Game_Interface:
         self.punktzahl = 0
         self.spieler_name = ''
     
-    def setName(self, stdscr_fn, curses_fn): pass
-
+    def setName(self, stdscr_fn, curses_fn):
+        c = stdscr_fn.getch()
+        if c != curses_fn.KEY_ENTER:
+            self.spieler_name += chr(c)
+            stdscr_fn.addstr(19, 10, self.spieler_name)
+            self.setName(stdscr_fn, curses_fn)
 
     def update_Punktzahl(self, cluster_f : ClusterFest):
         self.punktzahl = cluster_f.get_kompleteReihen()
@@ -14,18 +18,19 @@ class Game_Interface:
     def print_Interface(self, stdscr_fn):
         stdscr_fn.addstr(10, 55, f"Punktzahl: {self.punktzahl}")
 
-    def if_gameover(self,  cluster_f : ClusterFest): # Für GAMEOVER
+    def if_gameover(self,  cluster_f : ClusterFest):
         if cluster_f.pruef_ob_max_Hoehe():
             return True
         
-    def print_gameover(self, stdscr_fn,curses_fn): # Für GAMEOVER
+    def print_gameover(self, stdscr_fn,curses_fn):
         stdscr_fn.addstr(10, 1, "/////     //  //    //  /////   //  /     / ///// ////")
         stdscr_fn.addstr(11, 1, "//       / /  / /  / /  /___  /   /  /   /  /___  /  /")
         stdscr_fn.addstr(12, 1, "/  ///  ///// /  //  /  /     /   /   / /   /     ////")
         stdscr_fn.addstr(13, 1, "////   /   // // /   // /////  //      /    ///// /  // ")
 
         stdscr_fn.addstr(17, 10, f"Punktzahl: {self.punktzahl}")
-
+        stdscr_fn.addstr(18, 10, "Name:")
+        self.setName(stdscr_fn, curses_fn)
 
 
 
