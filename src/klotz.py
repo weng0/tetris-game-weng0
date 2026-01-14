897531ac7d6b5bdaeafd6f9e178d30238df42541
weng0 <73001331+weng0@users.noreply.github.com>
2026-01-14 16:50:11 +0100

    Add comments that describe all function of class Block.

--- src/klotz.py
+++ src/klotz.py
@@ -1,21 +1,26 @@
+# Das ist der atomare Bestandteil eines Tetrisblocks
+# Ein Block besteht aus zwei Zeilen, je gefüllt mit 3 Rauten
 class Block:
     def __init__(self):
-        self.x_pos = 0
-        self.y_pos = 0
-        self.zeilen = ['###','###']
+        self.x_pos = 0 # x-Koordinate des obersten rechten Teil des Blockes
+        self.y_pos = 0 # y-Koordinate des obersten rechten Teil des Blockes
+        self.zeilen = ['###','###'] # Der sichtbare Block besteht aus Rauten
 
+# Jede Zeile des Blockes hat eine Länge von 3
     def get_zeilen_laenge(self):
         return 3
 
-    def set_y(self, y):
-        self.y_pos = y
-
+# Diese Funktionen geben die Koordinaten des Blockes zurück
     def get_x(self):
         return self.x_pos
     
     def get_y(self):
         return self.y_pos
+    
+    def get_pos(self):
+        return self.y_pos, self.x_pos
 
+# Diese Funktionen geben die Koordinaten der Blockseiten zurück
     def get_U_Seite(self):
         return self.y_pos+1,self.x_pos
     
@@ -28,13 +33,15 @@ class Block:
     def get_O_Seite(self):
         return self.y_pos, self.x_pos
 
+# Diese Funktionen überschreiben die Koordinaten des Blockes
     def set_pos(self, y, x):
         self.y_pos = y
         self.x_pos = x
 
-    def get_pos(self):
-        return self.y_pos, self.x_pos
-    
+    def set_y(self, y):
+        self.y_pos = y
+
+# Diese Funktion malt ein Block auf dem Terminalfenster
     def draw(self, stdscr_fn):
         for zeilennummer in range(len(self.zeilen)):
             zeile = self.rows[zeilennummer]
